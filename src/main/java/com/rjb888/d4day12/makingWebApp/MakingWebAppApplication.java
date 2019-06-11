d3a6764511f02d24e4757242154aa884d795568c
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-06-11 16:07:39 -0700

    initial commit of springboot app

---
+++ src/main/java/com/rjb888/d4day12/makingWebApp/MakingWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.rjb888.d4day12.makingWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakingWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakingWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rjb888/d4day12/makingWebApp/MakingWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rjb888.d4day12.makingWebApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MakingWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
